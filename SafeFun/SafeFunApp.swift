c4b4f84795164c5d3107a5632d4e2e0100f83b6f
Gonzalo Cárdenas <lalo020404@gmail.com>
2025-10-19 16:00:13 -0600

    Initial Commit

---
+++ SafeFun/SafeFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeFunApp.swift
+//  SafeFun
+//
+//  Created by Lalo Cardenas on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
