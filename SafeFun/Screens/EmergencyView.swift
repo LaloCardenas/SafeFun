bff30a29ea90c53229d0752bfad39ffe3c42bcb1
Gonzalo Cárdenas <lalo020404@gmail.com>
2025-10-19 20:11:09 -0600

    Screeens created

---
+++ SafeFun/Screens/EmergencyView.swift
@@ -0,0 +1,18 @@
+//
+//  EmergencyView.swift
+//  SafeFun
+//
+//  Created by Lalo Cardenas on 19/10/25.
+//
+
+import SwiftUI
+
+struct EmergencyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmergencyView()
+}
---
+++ SafeFun/Screens/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  SafeFun
+//
+//  Created by Lalo Cardenas on 19/10/25.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
